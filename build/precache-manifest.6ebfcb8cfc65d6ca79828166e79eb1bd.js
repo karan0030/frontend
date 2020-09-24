888bebbab60a8379b6641b8463ee637fb9db2082
karan <karansagar30@gmail.com>
2020-09-25 01:37:42 +0530

    for hosting

---
+++ build/precache-manifest.6ebfcb8cfc65d6ca79828166e79eb1bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec7b5b6580438dda688ecd9330cfb17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09ce93224340a4c71439",
+    "url": "/static/css/main.52090a4f.chunk.css"
+  },
+  {
+    "revision": "17570f7c4b89c4397078",
+    "url": "/static/js/2.6ef226ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ef226ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ce93224340a4c71439",
+    "url": "/static/js/main.9491fb33.chunk.js"
+  },
+  {
+    "revision": "72017dee5443dfc32ffd",
+    "url": "/static/js/runtime-main.dc49cf19.js"
+  }
+]);
